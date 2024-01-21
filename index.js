97f8e6bf4dcc590b4739f6227adbdb69f547387e
JoshuaYoung1997 <151977540+JoshuaYoung1997@users.noreply.github.com>
2024-01-21 15:55:47 -0600

    Animation

---
+++ index.js
@@ -0,0 +1,11 @@
+function animateOut(){
+    var element = document.getElementById('popupButton');
+    element.classList.add('scale-out-center');
+    element.classList.remove('scale-out-center');
+}
+
+function animateIn(){
+    var button = document.getElementById('popupButton');
+    var modal = document.getElementById('popupButton');
+    button.classList.add('scale-out-center');
+}

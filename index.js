1f28f35ab41cbc628e3786b85a1aa781285a3bd3
JoshuaYoung1997 <151977540+JoshuaYoung1997@users.noreply.github.com>
2024-01-21 16:21:16 -0600

    Button Exiting Animation

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-function animateOut(){
+function animateButtonOut(){
     var element = document.getElementById('popupBox');
     element.classList.add('scale-out-center');
     setTimeout(() => {
-        console.log("Delayed for 1 second.");
+        element.classList.remove('scale-out-center');
       }, "1000");
 }
 
-function animateIn(){
+function animateButtonIn(){
     var element = document.getElementById('popupBox');
-    button.classList.add('scale-out-center');
+    button.classList.add('scale-in-center');
+    setTimeout(() => {
+        element.classList.remove('scale-in-center');
+      }, "1000");
+}
+
+function animateBoxOut(){
+    var element = document.getElementById('popupBox');
+    element.classList.add('scale-out-center');
+    setTimeout(() => {
+        element.classList.remove('scale-out-center');
+      }, "1000");
+}
+
+function animateBoxIn(){
+    var element = document.getElementById('popupBox');
+    button.classList.add('scale-in-center');
+    setTimeout(() => {
+        element.classList.remove('scale-in-center');
+      }, "1000");
 }

14553be6cb50e62a4445ffaf40178d671cc1595a
JoshuaYoung1997 <151977540+JoshuaYoung1997@users.noreply.github.com>
2024-01-21 16:35:40 -0600

    Removed Comments

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// function animateButtonOut(){
-//     var element = document.getElementById('popupBox');
-//     element.classList.add('scale-out-center');
-//     setTimeout(() => {
-//         element.classList.remove('scale-out-center');
-//       }, "1000");
-// }
-
-// function animateButtonIn(){
-//     var element = document.getElementById('popupBox');
-//     button.classList.add('scale-in-center');
-//     setTimeout(() => {
-//         element.classList.remove('scale-in-center');
-//       }, "1000");
-// }
-
-// function animateBoxOut(){
-//     var element = document.getElementById('popupBox');
-//     element.classList.add('scale-out-center');
-//     setTimeout(() => {
-//         element.classList.remove('scale-out-center');
-//       }, "1000");
-// }
-
-// function animateBoxIn(){
-//     var element = document.getElementById('popupBox');
-//     button.classList.add('scale-in-center');
-//     setTimeout(() => {
-//         element.classList.remove('scale-in-center');
-//       }, "1000");
-// }
-
 TweenLite.set(".slideModalBottom", { autoAlpha: 0, y: 50 });
 TweenLite.set(".modalOverlay", { autoAlpha: 0 });
 

86cb6f433cb937fc8c751b2e79370ab3dc148240
JoshuaYoung1997 <151977540+JoshuaYoung1997@users.noreply.github.com>
2024-01-21 16:10:43 -0600

    Testing Timeout

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function animateOut(){
     var element = document.getElementById('popupBox');
     element.classList.add('scale-out-center');
+    setTimeout(() => {
+        console.log("Delayed for 1 second.");
+      }, "1000");
 }
 
 function animateIn(){
